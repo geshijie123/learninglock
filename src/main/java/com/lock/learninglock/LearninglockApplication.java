abb6b663181498a0d230a334691b09cc6bc9129f
geshijie123 <1905548803@qq.com>
2019-01-17 09:30:33 +0800

    首次提交

---
+++ src/main/java/com/lock/learninglock/LearninglockApplication.java
@@ -0,0 +1,14 @@
+package com.lock.learninglock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearninglockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearninglockApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/lock/learninglock/LearninglockApplicationTests.java
@@ -0,0 +1,17 @@
+package com.lock.learninglock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LearninglockApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
